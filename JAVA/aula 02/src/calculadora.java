d870e4ef8b58342898b91c77cbcb84b1e92d7094
rafamoreira11 <rafaelmoreiralima50@alu.ufc.br>
2025-04-12 11:36:46 -0300

    upload

--- JAVA/aula 02/src/calculadora.java
+++ JAVA/aula 02/src/calculadora.java
@@ -1,8 +1,9 @@
 public class calculadora {
     public static void main(String[] args) {
-        int n1 = 13;
+        int n1 = 20;
         int n2 = 10;
-        System.out.println(n1 + n2);
+        int soma = n1 + n2;
+        System.out.println(soma);
     }
-    
+
 }
---
+++ JAVA/aula 03/src/condicionais.java
@@ -0,0 +1,13 @@
+public class condicionais{
+    public static void main(String[] args) {
+        boolean x = true;
+
+        if(x){
+            System.out.println("Verdadeiro");
+        } 
+        else{
+            System.out.println("Falso");
+        }
+    }
+    
+}
