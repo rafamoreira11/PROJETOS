12be07b49cc04e069b75a3aa618185f589c1caec
rafamoreira11 <rafaelmoreiralima50@alu.ufc.br>
2025-04-12 12:12:09 -0300

    upload

---
+++ JAVA/aula 04/src/vetores.java
@@ -0,0 +1,8 @@
+public class vetores{
+    public static void main(String[] args) {
+        String[] frutas = {"maçã", "banana", "melância", "uva"};
+
+        System.out.println(frutas[2]);
+    }
+    
+}
